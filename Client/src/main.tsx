57e86f77244c03859c2e209ec4ddf444efdabff0
Coulube <anchorvicky3@gmail.com>
2026-01-28 17:08:45 +0100

    Fix frontend build by standardizing on App.tsx

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,20 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { AuthProvider } from './context/AuthContext'
-import { Toaster } from './components/ui/sonner'
-import App from './App'
-import './index.css'
+﻿import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const queryClient = new QueryClient();
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <App />
-        <Toaster />
-      </AuthProvider>
-    </QueryClientProvider>
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
